f1a4deed5d236ba70b35b0e2d88953765b2f5325
luhem7 <mehul.gang@gmail.com>
2025-12-19 06:25:18 -0500

    Added a main and header component

---
+++ ca_server/frontend/src/components/Main.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Main() {
+  return (
+    <div className="main-container">
+      <canvas>
+        Your browser does not support the HTML5 canvas element.
+      </canvas>
+      <p>This is the Main component with placeholder text. The canvas element is ready for your interactive content!</p>
+    </div>
+  )
+}
+
+export default Main
